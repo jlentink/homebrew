1b8d08565a041a862d37cdcbbff6e26ef7504cc2
Jason Lentink <jason@mediamonks.com>
2019-10-22 17:16:09 +0200

    set correct validation signature

--- aem-cli.rb
+++ aem-cli.rb
@@ -3,7 +3,7 @@ class AemCli < Formula
   homepage "https://github.com/jlentink/aem"
   version "1.0.0rc10"
   url "https://github.com/jlentink/aem/archive/#{version}.zip"
-  sha256 "4660e1b2c865b48373fc21fab95800d895180b613a42ef424aaf71d83f147003"
+  sha256 "0bd0e59d48faf5709762ad62a1658451102184341b0be4dff4246988242eafe5"
   depends_on "go" => :build
 
   def install
