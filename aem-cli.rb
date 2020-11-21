8f34a628a589222feb97bd5e08d5e4312d968add
Jason Lentink <jason@mediamonks.com>
2020-11-21 01:42:20 +0100

    bump version

--- aem-cli.rb
+++ aem-cli.rb
@@ -1,9 +1,9 @@
 class AemCli < Formula
   desc "AEM command line interface"
   homepage "https://github.com/jlentink/aem"
-  version "1.0.0rc14"
+  version "1.0.0rc16"
   url "https://github.com/jlentink/aem/archive/#{version}.zip"
-  sha256 "79db9e787ac3a44349bb449ecdd3d10af8e35238e707a38a12ad45319f35fcf4"
+  sha256 "e89703df3cf6dd53a529ebbc0b672e5ea17446269e6f3463050708ad69e555bf"
   depends_on "go" => :build
 
   def install
